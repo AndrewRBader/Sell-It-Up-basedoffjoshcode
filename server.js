f8bfde9b9d998b583d984fa0e61f7f65f9c1f85a
RyanMacc <arbader2142@gmail.com>
2022-04-15 21:03:55 -0400

    added import and export from product model.js to server.js works in browser

--- server.js
+++ server.js
@@ -2,27 +2,10 @@ const express = require('express');
 const app = express();
 const PORT = 4000;
 
-// temporary, simulated database 
-const products = [
-  {
-    name: 'Internet Friends',
-    price: 29,
-    image:
-      'https://cdn.shopify.com/s/files/1/1297/1509/products/hero1_6de889fb-b540-49e4-b733-3af0baaa7f63_x1440.jpg?v=1571274629',
-  },
-  {
-    name: 'Angry Pants',
-    price: 35,
-    image:
-      'https://cdn.shopify.com/s/files/1/1297/1509/products/HERO_c5b0ec76-ad06-4cc7-a165-6129e11a8ff6_x1440.jpg?v=1571274622',
-  },
-  {
-    name: 'Dead Cool',
-    price: 50,
-    image:
-      'https://cdn.shopify.com/s/files/1/1297/1509/products/hero1_40030160-f468-4d50-8f30-c8b9733ce84e_x1440.jpg?v=1575020412',
-  },
-];
+// getting products here via import and storing in variable
+
+const products = require('./models/product_model.js')
+console.log(products)
 
 // index route 
 // this route will catch GET requests to /products/ and respond with all the products
