01fe84bc3ab03d358f20b6ca3b299271ac508b97
RyanMacc <arbader2142@gmail.com>
2022-04-15 21:22:51 -0400

    everything works up to ejs part

--- server.js
+++ server.js
@@ -5,18 +5,25 @@ const PORT = 4000;
 // getting products here via import and storing in variable
 
 const products = require('./models/product_model.js')
-console.log(products)
+// console.log(products)
 
-// index route 
-// this route will catch GET requests to /products/ and respond with all the products
-app.get('/products/', (req, res) => { 
-	res.send(products);
-});
+//product show route - GET - one product
 
-// show route 
-// this route will catch GET requests to /products/index/ and respond with a single product
-app.get('/products/:productIndex', (req, res) => { 
-	res.send(products[req.params.productIndex]); 
-});
+app.get('/products/:productId/', (req,res) => {
+    let productId = req.params.productId;
+    console.log(productId);
+    res.send(products[productId]);
+})
 
+// product index route, get all the products
+app.get('/products', (req, res) => {
+    res.send(products);
+})
+
+//home route '/'
+app.get('/', (request, response) =>{
+    response.send('Welcome to Sell-it-Up')
+})
+
+// express server: initializes the server, app.listen allows computer to receive requests at http://localhost:4000/
 app.listen(PORT, () => {  console.log(`Listening for client requests on port ${PORT}`);})
